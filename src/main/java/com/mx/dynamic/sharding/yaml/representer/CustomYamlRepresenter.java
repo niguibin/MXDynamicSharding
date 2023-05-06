26124f91b1cd6961349419186415a080575ec88f
niguibin <1769768683@qq.com>
2023-05-06 17:03:45 +0800

    update

--- src/main/java/com/mx/dynamic/sharding/yaml/representer/CustomYamlRepresenter.java
+++ src/main/java/com/mx/dynamic/sharding/yaml/representer/CustomYamlRepresenter.java
@@ -1,5 +1,6 @@
 package com.mx.dynamic.sharding.yaml.representer;
 
+import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.introspector.Property;
 import org.yaml.snakeyaml.nodes.NodeTuple;
 import org.yaml.snakeyaml.nodes.Tag;
@@ -10,6 +11,11 @@ import org.yaml.snakeyaml.representer.Representer;
  * @date: 2022/8/10 4:10 下午
  */
 public final class CustomYamlRepresenter extends Representer {
+
+    public CustomYamlRepresenter() {
+        super(new DumperOptions());
+    }
+
     @Override
     protected NodeTuple representJavaBeanProperty(Object javaBean, Property property, Object propertyValue, Tag customTag) {
         return new DefaultYamlTupleProcessor().process(super.representJavaBeanProperty(javaBean, property, propertyValue, customTag));
